adda392233da49246d7c8df89454770363957f79
hyena <goatsfish@gmail.com>
2024-06-29 09:04:41 +0900

    init

---
+++ src/main/kotlin/com/hyena/selffinance/SelfFinanceApplication.kt
@@ -0,0 +1,11 @@
+package com.hyena.selffinance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SelfFinanceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SelfFinanceApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyena/selffinance/SelfFinanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyena.selffinance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SelfFinanceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
